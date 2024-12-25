193223b2ee6265097f35347c69d6fae46c944d71
mathyber <ilan23f@gmail.com>
2024-12-25 20:50:02 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SongBlock from "./components/songBlock";
+
+function App() {
+    const index = 0;
+    return (
+        <>
+            <SongBlock index={index}/>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/songBlock.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SongBlock = ({index}) => {
+    return (
+        <div>
+            {index}
+        </div>
+    );
+};
+
+export default SongBlock;
